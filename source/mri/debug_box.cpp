259abb7c9a223706d22208c8f2dd34465a71a2a1
JaredCJR <empire89090@gmail.com>
2016-02-02 22:33:28 +0800

    Try to save general registers in USART3_Handler.
    In thumb mode,You can only access r0~r7,r13~r15.

--- source/mri/debug_box.cpp
+++ source/mri/debug_box.cpp
@@ -20,9 +20,50 @@ DEBUG_BOX_ACL(g_debug_box_acl);
 /* configure secure box compartment and reserve box context */
 UVISOR_BOX_CONFIG(debug_box, g_debug_box_acl, UVISOR_BOX_STACK_SIZE, MriCore);
 
-void mri_Handler(void)                                                                                                          
+
+/*For test,the code dose not make any sense.*/
+#define debugbreak()  { __asm volatile ("bkpt #0"); }
+void volatile mri_Handler(void) 
 {
+    uint32_t registers[16];
     uvisor_ctx->mri_serial->printf("mri_Handler\n\r");
+
+    /*Try to access registers*/
+    asm volatile(
+            "mov %[var_r0],r0\n\t"
+            "mov %[var_r1],r1\n\t"
+            "mov %[var_r2],r2\n\t"
+            "mov %[var_r3],r3\n\t"
+            "mov %[var_r4],r4\n\t"
+            "mov %[var_r5],r5\n\t"
+            "mov %[var_r6],r6\n\t"
+            "mov %[var_r7],r7\n\t"
+            "mov %[var_sp],sp\n\t"
+            "mov %[var_lr],lr\n\t"
+            "mov %[var_pc],pc\n\t"
+            :[var_r0]"=r"(registers[0]),
+             [var_r1]"=r"(registers[1]),
+             [var_r2]"=r"(registers[2]),
+             [var_r3]"=r"(registers[3]),
+             [var_r4]"=r"(registers[4]),
+             [var_r5]"=r"(registers[5]),
+             [var_r6]"=r"(registers[6]),
+             [var_r7]"=r"(registers[7]),
+             [var_sp]"=r"(registers[13]),
+             [var_lr]"=r"(registers[14]),
+             [var_pc]"=r"(registers[15])
+            :
+            :);
+
+    for(int i =0;i<16;i++)
+    {
+        uvisor_ctx->mri_serial->printf("r[%d]=0x%.8X\r\n",i,registers[i]);
+        if(i==7)
+        {
+            i = 12;
+        }
+    }
+    uvisor_ctx->mri_serial->printf("-------------------------------------\n\r");
     mriClearRxInterrupt(USART3);//USART3
 }
 

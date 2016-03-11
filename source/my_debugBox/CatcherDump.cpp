673e5325eb0d5010f7a69a3b1b1bf60fb86f0785
JaredCJR <empire89090@gmail.com>
2016-03-11 16:59:10 +0800

    Customized printf created.

--- source/my_debugBox/CatcherDump.cpp
+++ source/my_debugBox/CatcherDump.cpp
@@ -1,12 +1,24 @@
 #include "CrashCatcher.h"
 #include "CatcherDump.h"
+#include <stdarg.h>
 
 Serial uart(STDIO_UART_TX, STDIO_UART_RX);
+
+static void cC_printf(const char * format,...)
+{    
+    va_list args;
+    va_start(args, format);
+
+    uart.vprintf(format, args);
+
+    va_end(args);
+}
 
 
 void CrashCatcher_DumpStart(void) 
 {
     uart.baud(115200);
-    uart.printf("\r\n\r\nCRASH ENCOUNTERED\r\n"
+    cC_printf("\r\n\r\nCRASH ENCOUNTERED\r\n"
                  "Enable logging and then press any key to start dump.\r\n");
 }
+
